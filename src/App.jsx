10a5126243c61dbd59fc146d6fc71c3fa4ba436d
Vanessa Blaho <vanessadblaho@gmail.com>
2024-02-27 20:46:48 +0100

    completed investment calculator with dynamic values

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
 import Results from "./components/Results";
-import UserInput from "./components/UserInput"
+import UserInput from "./components/UserInput";
 import { useState } from "react";
 
 function App() {
-  const [userInput,setUserInput] = useState({
+  const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
-    duration:10
-
+    duration: 10,
   });
-  function handleChange (inputIdentifier,newValue){
-    setUserInput(prevUserInput =>{
-      return{
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
         ...prevUserInput,
-        [inputIdentifier]:newValue
+        [inputIdentifier]: +newValue,
       };
     });
-  };
+  }
   return (
     <>
-  <Header/>
-  <UserInput
-  onChange={handleChange}
-  userInput={userInput}/>
-  <Results
-  input={userInput}/>
-  </>
-  )
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && <p className="center"> Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
